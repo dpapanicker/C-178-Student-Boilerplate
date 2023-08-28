8e3e88f02cec40844d08510f3ce78e61c393c910
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2023-08-28 16:28:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let latitude=22.7868542, longitude=88.3643296;
+
+// Initializing Mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+//Map Object- to render the maps on the web page
+ 
+
+
+
+// add controls in the Mapbox maps
+map.addControl(
+	
+
+);
+
+map.addControl(
+	
+
+	
+);
+
+   
